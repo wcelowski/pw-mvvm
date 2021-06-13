fa81d8af9e091e7c6aaa92b535221b834e38a17f
wcelowski <wcelowski@gog.com>
2021-06-13 14:47:56 +0200

    wip v1

--- src/viewModels/todoViewModel.ts
+++ src/viewModels/todoViewModel.ts
@@ -3,20 +3,35 @@ import todoListModel from "../models/todoListModel";
 import Observable from "../infrastructure/observable";
 
 const main = () => {
-    applyBindings();
+    applySubscribers();
     applyMounted();
+    applyDataBindings();
+    applyCommandHandlers();
 }
 
-let data: { isLoading: Observable; todoList: Observable; todoListItems: Observable };
-data = {
+type Data = { [key: string]: Observable }
+const data: Data = {
     isLoading: new Observable(false),
     todoList: new Observable(null),
     todoListItems: new Observable([]),
+    newItem: new Observable(''),
 };
 
-const applyBindings = () => {
-    data.todoListItems.subscribe((values: any) => applyForEach(values));
+const applySubscribers = () => {
+    data.todoList.subscribe((value: todoListModel) => applyForEach(value.getItems()));
+    // data.todoListItems.subscribe((values: any) => applyForEach(values));
     data.isLoading.subscribe((isLoading: boolean) => handlers.initializeLoader(isLoading))
+}
+
+const applyDataBindings = () => {
+    document.querySelectorAll("[data-bind]").forEach(element => {
+        if (element instanceof HTMLInputElement) {
+            const observable = data[element.getAttribute("data-bind")];
+            element.value = observable.getValue();
+            observable.subscribe(() => element.value = observable.getValue());
+            element.onkeyup = () => observable.setValue(element.value);
+        }
+    });
 }
 
 const handlers = {
@@ -24,16 +39,18 @@ const handlers = {
         data.isLoading.setValue(true);
         const todoList = await (inMemoryToDoRepository.find() as Promise<todoListModel>);
         data.todoList.setValue(todoList);
-        const todoListItems = todoList.getItems();
-        data.todoListItems.setValue(todoListItems);
+        // const todoListItems = todoList.getItems();
+        // data.todoListItems.setValue(todoListItems);
         data.isLoading.setValue(false);
     },
     initializeLoader: (isLoading: boolean) => {
-        console.log(isLoading);
         document.getElementById('todo-loader')
-            // .setAttribute('style', `display: none`);
             .setAttribute('style', `display: ${isLoading ? 'block' : 'none'}`);
-        console.log(isLoading);
+    },
+    addItem: () => {
+        data.todoList.setValue(
+            new todoListModel([...data.todoListItems.getValue(), data.newItem.getValue()])
+        );
     }
 }
 
@@ -41,28 +58,32 @@ const mounted = {
     fetchToDoList: handlers.fetchToDoList
 }
 
-// const bindValue = (input: HTMLInputElement, observable: Observable) => {
-//     input.value = observable.getValue();
-//     observable.subscribe(() => input.value = observable.getValue());
-//     input.onkeyup = () => observable.setValue(input.value);
-// }
-
 const applyMounted = async () => {
     mounted.fetchToDoList.call(data);
 }
 
+const applyCommandHandlers = () => {
+    document.querySelectorAll('[on-click]')
+        .forEach(element => {
+            const prop = element.getAttribute('on-click');
+            // @ts-ignore
+            element.addEventListener('click', handlers[prop], false)
+        })
+}
+
 const applyForEach = (values: Array<any>) => {
+    console.log(values);
     document.querySelectorAll('[for-each]')
         .forEach(element => {
             const prop = element.getAttribute('for-each')
             // @ts-ignore
-            if ((data[prop] as Observable).getValue() === values) {
+            // if ((data[prop] as Observable).getValue() === values) {
                 let foreachHtml: string = '';
                 values.forEach(value => {
                     foreachHtml += `<li>${value}</li>`
                 })
                 element.innerHTML += `<ul>${foreachHtml}</ul>`
-            }
+            // }
         })
 }
 
